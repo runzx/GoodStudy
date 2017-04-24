7b4dde25e158b15002dfc9b18f43913f5065b696
zhaix <runzx@hotmail.com>
2017-04-24 08:08:00 +0800

    加拿大签证清单， TP5学习笔记， PHPstorm使用及配置

---
+++ PHP/RESTapi.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhaix
+ * Date: 2017/4/22
+ * Time: 11:37
+ */
+/*  状态码	含义	说明
+    200	OK	请求成功
+    201	CREATED	创建成功
+    202	ACCEPTED	更新成功
+    400	BAD REQUEST	请求的地址不存在或者包含不支持的参数
+    401	UNAUTHORIZED	未授权
+    403	FORBIDDEN	被禁止访问
+    404	NOT FOUND	请求的资源不存在
+    500	INTERNAL SERVER ERROR	内部错误*/
+
+//https://developers.douban.com/wiki/?title=api_v2  豆瓣API
+//https://developer.github.com/v3/      Github API
