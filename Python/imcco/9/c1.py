dec1e2740501aa0d713ee5ac4c825dd6d8c73b2a
zhaix <runzx@hotmail.com>
2017-10-18 08:26:05 +0800

    类和对象

---
+++ Python/imcco/9/c1.py
@@ -0,0 +1,16 @@
+'''
+类=面向对象
+类、对象
+实例化
+类的基本作用：封装
+类的方法只能在类外面执行
+方法： 设计层面
+函数： 运行，过程
+'''
+
+class Student():
+    name=''
+    age=0
+
+    def print_file(self, parameter_list):
+        raise NotImplementedError
