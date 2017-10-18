83d6824c5e8a6e1b275e215895627d1750e35750
zhaix <runzx@hotmail.com>
2017-10-18 09:43:02 +0800

    pyLint  Message

--- Python/imcco/9/c1.py
+++ Python/imcco/9/c1.py
@@ -12,21 +12,30 @@ class Student():
     name=''
     age=0
 
+    #
+    def __init__(self,name,age):
+        #构造函数
+        #初始化对象
+        name = name
+        age = age
+        print('student name: '+ name + ',  age: '+str(age))
+        pass
     #行为
     def do_homework(self ):
         print('homework')
 
+'''
 class Printer():
-    '''
-    打印档案主体不应是学生对象
-    '''
+
+    #打印档案主体不应是学生对象
     
     #行为 与 特征
     def print_file(self, parameter_list):
         raise NotImplementedError
+'''
 
-student1 = Student()
-student2 = Student()
+student1 = Student('zhaixiang',20)
+student2 = Student('mm',7)
 
-print(id(student1))
-print(id(student2))
+# print(id(student1))
+# print(id(student2))
