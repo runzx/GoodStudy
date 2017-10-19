2556cfa7f97fd05ccc58968ec2410353a10fd0a1
zhaix <runzx@hotmail.com>
2017-10-19 20:34:14 +0800

    python for用法

--- Python/python_for.py
+++ Python/python_for.py
@@ -1,11 +1,27 @@
 '''
-
+for
 主要用于遍历、循环 序列或集合、字典
 
 '''
-for target_list in expression_list:
-    pass
+# for target_list in expression_list:
+#     pass
 
 # 重复的循环：for each
-for x in range(0,10):
-    print(x)
+for x in range(0,10,2):     # range(10,0,-2)
+    print(x, end=' | ')     # end='' 不换行！
+
+a = [1,2,3,4,5,6,7,8]
+# 打印间隔
+print('\n打印间隔 方法1')
+for x in a:
+    if x%2 == 1:
+        print(x, end=' | ')
+
+# 方法2
+print('\n方法2')
+for x in range(0, len(a), 2):
+    print(a[x], end=' | ')
+# 方法3 列表切片？
+print('\n方法3')
+b = a[0:len(a):2]
+print(b)
