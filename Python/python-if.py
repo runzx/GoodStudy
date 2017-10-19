b19b70bf53e052a72dc309a682bc2e017954c5e4
zhaix <runzx@hotmail.com>
2017-10-19 20:16:02 +0800

    python if语法

---
+++ Python/python-if.py
@@ -0,0 +1,19 @@
+'''
+python if else elif
+'''
+
+if condition:
+    pass
+else:
+    pass
+
+if condition:
+    pass
+elif expression:
+    pass
+elif expression:
+    pass
+else:
+    pass
+
+python 没有switch语法！
---
+++ Python/python_for.py
@@ -0,0 +1,11 @@
+'''
+
+主要用于遍历、循环 序列或集合、字典
+
+'''
+for target_list in expression_list:
+    pass
+
+# 重复的循环：for each
+for x in range(0,10):
+    print(x)
