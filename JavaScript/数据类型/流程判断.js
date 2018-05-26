31009ab4d6a705f5e1b50164abc7fe193b9de6e9
zhaixiang <843476168@qq.com>
2018-05-27 00:46:17 +0800

    JS case 判断，更有效的模式 ssh 转发nginx 配置

---
+++ JavaScript/数据类型/流程判断.js
@@ -0,0 +1,21 @@
+// switch case要写出很长的代码，造成不美观和难以维护
+
+var resType;
+var arrKey=[".html","",".js",".css",".gif",".jpg",".png",".ico"];
+var arrValue=["text/html","text/html","text/javascript","text/css","image/gif","image/jpeg","image/png","image/icon"]
+arrKey.map(function(key,index){
+   if(extname==key){
+       resType=arrValue[index];
+   }
+})
+
+// 或者：
+
+var resType;
+var arrKey=[".html","",".js",".css",".gif",".jpg",".png",".ico"];
+var arrValue=["text/html","text/html","text/javascript","text/css","image/gif","image/jpeg","image/png","image/icon"]
+arrKey.forEach(function(key,index){
+   if(extname==key){
+       resType=arrValue[index];
+   }
+})
