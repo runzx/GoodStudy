2383aa2f8432e2d73db10418b64bb198631e5e7b
zhaixiang <843476168@qq.com>
2018-05-31 00:06:09 +0800

    Js 学习， 判断空，arr预处理。 模块循环加载

---
+++ JavaScript/数据类型/Arry/arr预处理-map-filter-v.js
@@ -0,0 +1,19 @@
+
+var helpers = {
+    // 判断是否有时间参数
+    hasTime: (i) => {
+        return !isNaN(parseInt(i.stamp));
+    },
+    // 时间转换处理
+    parseTime: (i) => {
+        i.time = new Date(i.stamp + '000');
+        return i;
+    }
+};
+net.get('/Index/getList', (res = {}) => {
+    let {status, data: {list}} = res;
+    this.set({
+        list: list.filter(helpers.hasTime)  // 筛选掉没有时间戳字段的数据
+                  .map(helpers.parseTime)   // 将时间戳字段转化为 JS 的 Date 对象
+    });
+});
