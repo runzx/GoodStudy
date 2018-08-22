8170011c6070c7757e97d3ff2742a8f0b7a3982b
runzx <runzx@hotmail.com>
2018-08-23 07:49:59 +0800

    ubuntu robo3T安装 mongodb转换类型文档 pi3 9.4版卡机， 无线配置 vscode markdownlint 插件 报错表

--- DB/NoSql/mongodb/类型转换.js
+++ DB/NoSql/mongodb/类型转换.js
@@ -22,3 +22,31 @@ db.law.find().forEach(function (x) {
     x.eift = new ISODate(x.eift);
     db.law.save(x);
 });
+
+/**
+运行以下命令：
+db.文档.find({"列":{$type:Type Number}}).forEach(
+function(x){
+x.列=new NumberInt(x.列);
+db.文档.save(x);
+}
+);
+
+Type Number Type Explanation
+1 Double 浮点型
+2 String UTF-8字符串都可表示为字符串类型的数据
+3 Object 对象，嵌套另外的文档
+4 Array 值的集合或者列表可以表示成数组
+5 Binary data 二进制
+7 Object id 对象id是文档的12字节的唯一 ID 系统默认会自动生成
+8 Boolean 布尔类型有两个值TRUE和FALSE
+9 Date 日期类型存储的是从标准纪元开始的毫秒数。不存储时区
+10 Null 用于表示空值或者不存在的字段
+11 Regular expression 采用js 的正则表达式语法
+13 JavaScript code 可以存放Javasript 代码
+14 Symbol 符号
+15 JavaScript code with scope
+16 32-bit integer 32位整数类型
+17 Timestamp 特殊语义的时间戳数据类型
+18 64-bit integer 64位整数类型
+ */
