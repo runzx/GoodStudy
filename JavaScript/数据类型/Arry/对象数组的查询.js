2dff89e6d44ad58a6c2ffc6200a8da7258ea0c8f
zhaix <runzx@hotmail.com>
2019-01-29 01:38:35 +0800

    mongoose 更新操作update

---
+++ JavaScript/数据类型/Arry/对象数组的查询.js
@@ -0,0 +1,16 @@
+// 1. ES6 
+arr.find((element) => (element.name == '李四'))
+let obj = arr.find(o => o.name === 'string 1')
+// 2. 更高阶的功能，假设按’field’属性搜索：
+var elementPos = array.map(function(x) {return x.id; })
+  .indexOf(idYourAreLookingFor)
+var objectFound = array[elementPos]
+
+// 3.遍历数组并测试该属性：
+function search(nameKey, myArray){
+    for (var i=0; i < myArray.length; i++) {
+        if (myArray[i].name === nameKey) {
+            return myArray[i];
+        }
+    }
+}
