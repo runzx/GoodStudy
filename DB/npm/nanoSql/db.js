6bca016dc85e0c10298901785ded13d482823aba
zhaix <runzx@hotmail.com>
2019-09-18 22:11:59 +0800

    nanoSql 的snapDB 用child_prcess.fork(), 调试要特殊配置

--- DB/npm/nanoSql/db.js
+++ DB/npm/nanoSql/db.js
@@ -33,3 +33,12 @@ RocksDB     第三方                         10GB+
 // SnapDB是一个纯javascript持久键值存储，提供从键到字符串值的有序映射。使用受LevelDB / RocksDB启发的日志结构合并树（LSM树）将数据持久保存到磁盘。SnapDB与LevelDB和RocksDB具有100％的API兼容性，还包括其他功能。
 // <10GB
 // The SnapDB store isn't good for high volume, production systems. It's recommended you switch to LevelDB or RocksDB
+
+嵌入式数据库RocksDB是Facebook基于LevelDB开发的一种嵌入式Key-value存储系统，该数据库能够充分利用闪存的性能，大大提升应用服务器的速度。
+
+Rocksdb. 这个开源引擎是基于 Google 的 leveldb 1.5 版本, 但据称做了许多优化, 性能相对 leveldb 有了很大的提升, 而且解决了 leveldb 主动限制写的问题.
+
+nodejs PERM 就是 SNP -> snapDB ,其用了precss.fork()
+
+child_process.fork(path.join(__dirname, "database.js"),{execArgv: []});
+这样vscode才能正常调试
