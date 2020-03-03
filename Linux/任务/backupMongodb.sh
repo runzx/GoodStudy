122f50eebf5a992b581615184f95ae8b54211232
zhaixiang <843476168@qq.com>
2020-03-04 01:36:45 +0800

    上传 OSS

--- Linux/任务/backupMongodb.sh
+++ Linux/任务/backupMongodb.sh
@@ -11,9 +11,12 @@ mongodump -h localhost:27017 -d $db -o $backupFileName
 tar zcvf $backupFileName.tar.gz $backupFileName
 rm -rf $backupFileName
 
+NODE_ENV=$backupFolder@$backupFileName node /home/tasks/uploadOSS.js
+
 db=student
 backupFileName=$db-$dateNow
 mkdir -p $backupFileName
 mongodump -h localhost:27017 -d $db -o $backupFileName
 tar zcvf $backupFileName.tar.gz $backupFileName
 rm -rf $backupFileName
+
