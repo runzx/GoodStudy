1816a2ff04d1e1fecfa155da0da3c6f2609560e8
zhaixiang <843476168@qq.com>
2021-01-25 18:20:43 +0800

    feat : npm

---
+++ code/IDE/vscode/setup/空格配置.js
@@ -0,0 +1,12 @@
+ini = {
+  // The number of spaces a tab is equal to. This setting is overriden
+  // based on the file contents when `editor.detectIndentation` is true.
+  "editor.tabSize": 4,
+
+  // 按Tab键时插入空格
+  "editor.insertSpaces": true,
+
+  // When opening a file, `editor.tabSize` and `editor.insertSpaces`
+  // 启用时根据文件内容进行重写
+  "editor.detectIndentation": false
+}
