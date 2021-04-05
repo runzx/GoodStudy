e351b01690899ba490fd554cebdce5d6584914d7
zhaixiang <843476168@qq.com>
2021-04-05 17:56:19 +0800

    fmt

--- code/Python/imcco/7/c1.py
+++ code/Python/imcco/7/c1.py
@@ -4,11 +4,12 @@ for 循环
 '''
 a = [['apple', 'orange', 'banana', 'grape'], (1, 2, 3)]
 for x in a:
-    sum = 0
-    for y in x:
-        sum += 1
-        print(y, end=' | ')
-        if len(x) == sum:
-            print('\n\nfruit is gone')
+
+  sum = 0
+  for y in x:
+    sum += 1
+    print(y, end=' | ')
+    if len(x) == sum:
+      print('\n\nfruit is gone')
 
 Exception()
