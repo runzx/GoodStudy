b0dd49329077f40fd2f5f4016ebf2d56b9b5b762
zhaixiang <843476168@qq.com>
2021-04-06 11:03:16 +0800

    feat:  __file__ win/linux 不同

--- Python/study/imooc/c1.py
+++ Python/study/imooc/c1.py
@@ -2,19 +2,20 @@
 第一个学习例程
 '''
 import sys
-import s7
-# from s7.c2 import *
-from s7.c2 import b, a
-print('hello,world')
-True
-False
-None
+# import s7
+# # from s7.c2 import *
+# from s7.c2 import b, a
+# print('hello,world')
+# True
+# False
+# None
 
-# from 7 import c2
-# import c2
-print(a)
-print(b)
-# print(c2.a)
+# # from 7 import c2
+# # import c2
+# print(a)
+# print(b)
+# # print(c2.a)
 print(sys.path)
 print(__name__)
 print(__file__)
+print(__doc__)
